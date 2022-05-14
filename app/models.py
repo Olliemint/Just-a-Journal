afc78e3ce274e33e90080d0fce2b6d7d268b8425
Olliemint <furywrld5@gmail.com>
2022-05-14 16:49:01 +0300

    feat:created Blog  model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from re import T
 from app.models import db
 
 
@@ -6,8 +7,16 @@ class User(db.Model):
     username = db.Column(db.String,unique=True ,nullable=False)
     email = db.Column(db.String,unique=True,nullable=False)
     password = db.Column(db.String,nullable =False)
+    posts = db.relationship("Blog", backref='user', lazy =True)
     
     
     def __repr__(self):
         
         return f"User('{self.username}','{self.email}')"
+    
+class Blog(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String,nullable=False)
+    category = db.Column(db.String,nullable=False)
+    content = db.Column(db.String,nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)    
